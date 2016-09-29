34a6f203242c9d2037691be0478c5130d5fd0098
OKchesterOK <igor.ivanov.89@gmail.com>
2016-09-29 22:43:16 +0300

    test

--- temp.js
+++ temp.js
@@ -1,23 +1,49 @@
-function showGoogleMaps() {
-    var mapOptions = {
-        zoom: 16,
-        streetViewControl: false,
-        scaleControl: true,
-        mapTypeControl: false,
-        mapTypeId: google.maps.MapTypeId.ROADMAP,
-        center: {lat: 50.458509281147066, lng: 30.42147965456450}
-    };
-    map = new google.maps.Map(document.getElementById('googlemaps'), mapOptions);
-    marker = new google.maps.Marker({
-        position: {lat: 50.458509281147066, lng: 30.42351667728076},
-        map: map,
-        draggable: false
+(function () {
+
+    var
+        map,
+
+        marker,
+
+        markerPosition = {
+            lat: 50.458509281147066,
+            lng: 30.42351667728076
+        },
+
+        mapPosition = {
+            lat: 50.458509281147066,
+            lng: 30.42147965456450
+        };
+
+    function showGoogleMaps() {
+
+        var mapOptions = {
+            zoom: 16,
+            streetViewControl: false,
+            scaleControl: true,
+            mapTypeControl: false,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            center: mapPosition
+        };
+
+        map = new google.maps.Map(document.getElementById('googlemaps'), mapOptions);
+
+        marker = new google.maps.Marker({
+            position: markerPosition,
+            map: map,
+            draggable: false
+        });
+    }
+
+    google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+
+    $(window).resize(function() {
+        initializeMap();
     });
-}
 
-google.maps.event.addDomListener(window, 'load', showGoogleMaps);
+    function initializeMap() {
+        marker.position = new google.maps.LatLng(mapPosition.lat, mapPosition.lng);
+        marker.setMap(map);
+    }
 
-$(window).resize(function() {
-    location.reload();
-});
-
+})();
