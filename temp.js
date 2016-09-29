e2f66601cbf1874446d64dd3d75d1e41cf184056
OKchesterOK <igor.ivanov.89@gmail.com>
2016-09-29 20:35:04 +0300

    test Js8

--- temp.js
+++ temp.js
@@ -18,6 +18,6 @@ function showGoogleMaps() {
 google.maps.event.addDomListener(window, 'load', showGoogleMaps);
 
 $(window).resize(function() {
-    getElementById('googlemaps').location.reload();
+    location.reload();
 });
 
